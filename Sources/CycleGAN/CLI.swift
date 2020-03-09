73f23b2ec1cde111a60d7b5da6d99457c2f71df5
s1ddok <siddok@gmail.com>
2020-03-09 21:00:20 +0300

    move cli options to a dedicated file

---
+++ Sources/CycleGAN/CLI.swift
@@ -0,0 +1,15 @@
+import ArgumentParser
+
+struct Options: ParsableArguments {
+    @Option(default: "/notebooks/avolodin/data/horce2zebra/", help: ArgumentHelp("Path to the dataset folder", valueName: "dataset"))
+    var datasetPath: String
+    
+    @Option(default: 3, help: ArgumentHelp("GPU Index", valueName: "gpu-index"))
+    var gpuIndex: UInt
+    
+    @Option(default: 200, help: ArgumentHelp("Number of epochs", valueName: "epochs"))
+    var epochs: Int
+    
+    @Option(default: "/tmp/tensorboardx", help: ArgumentHelp("TensorBoard logdir path", valueName: "tensorboard-logdir"))
+    var tensorboardLogdir: String
+}

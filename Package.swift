b698c1487eb23defe93badd441f85a9ed01f507f
Andrey Volodin <siddok@gmail.com>
2020-03-12 18:00:01 +0300

    update package file

--- Package.swift
+++ Package.swift
@@ -1,18 +1,22 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CycleGAN",
+    platforms: [.macOS(SupportedPlatform.MacOSVersion.v10_13)],
+    products: [
+        .executable(name: "CycleGAN", targets: ["CycleGAN"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
-        .package(url: "https://github.com/t-ae/tensorboardx-s4tf.git", from: "0.1.1"),
+        .package(name: "TensorBoardX", url: "https://github.com/t-ae/tensorboardx-s4tf.git", from: "0.1.2"),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
     ],
     targets: [
         .target(
             name: "CycleGAN",
-            dependencies: ["Files", "TensorBoardX", "ArgumentParser"]),
+            dependencies: ["Files", "TensorBoardX", .product(name: "ArgumentParser", package: "swift-argument-parser")])
     ]
 )

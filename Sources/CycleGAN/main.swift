b0ed08080dd5fcec080f15e56fb1d6216dc3b21c
Andrey Volodin <siddok@gmail.com>
2020-03-12 18:01:30 +0300

    robust losing, final test run, intermediate image logging

--- Sources/CycleGAN/main.swift
+++ Sources/CycleGAN/main.swift
@@ -4,14 +4,17 @@ import Foundation
 import TensorBoardX
 
 let options = Options.parseOrExit()
+let logDirURL = URL(fileURLWithPath: options.tensorboardLogdir, isDirectory: true)
+let runId = currentRunId(logDir: logDirURL)
+let writerURL = logDirURL.appendingPathComponent(String(runId), isDirectory: true)
+let writer = SummaryWriter(logdir: writerURL)
 
-let logdir = URL(fileURLWithPath: options.tensorboardLogdir).appendingPathComponent(String(Int(Date().timeIntervalSince1970)))
-//try? FileManager.default.removeItem(at: logdir)
-let writer = SummaryWriter(logdir: logdir)
+let datasetFolder = try Folder(path: options.datasetPath)
+let trainFolderA = try datasetFolder.subfolder(named: "trainA")
+let trainFolderB = try datasetFolder.subfolder(named: "trainB")
+let testFolderA = try datasetFolder.subfolder(named: "testA")
+let testFolderB = try datasetFolder.subfolder(named: "testB")
 
-let facadesFolder = try Folder(path: options.datasetPath)
-let trainFolderA = try facadesFolder.subfolder(named: "trainA")
-let trainFolderB = try facadesFolder.subfolder(named: "trainB")
 let trainDatasetA = try Images(folder: trainFolderA)
 let trainDatasetB = try Images(folder: trainFolderB)
 
@@ -32,6 +35,10 @@ let zeros = Tensorf.zero
 let ones = Tensorf.one
 let gpuIndex = options.gpuIndex
 
+var step = 0
+
+let sampleImage = Image(jpeg: testFolderA.files.first!.url).tensor / 127.5 - 1
+
 for epoch in 0..<epochs {
     print("Epoch \(epoch) started at: \(Date())")
     Context.local.learningPhase = .training
@@ -43,16 +50,9 @@ for epoch in 0..<epochs {
                                          .shuffled(sampleCount: trainDatasetB.count,
                                                    randomSeed: Int64(epoch))
     let zippedAB = zip(trainingAShuffled, trainingBShuffled)
-
-    var ganGLossTotal = Tensorf(0)
-    var ganFLossTotal = Tensorf(0)
-    
-    var discXLossTotal = Tensorf(0)
-    var discYLossTotal = Tensorf(0)
-    
-    var totalLossCount: Float = 0
     
     for batch in zippedAB.batched(batchSize) {
+        // MARK: Test
         Context.local.learningPhase = .training
         let realX = batch.first.image
         let realY = batch.second.image
@@ -74,110 +74,125 @@ for epoch in 0..<epochs {
             let onesd = ones.broadcasted(to: [1, 30, 30, 1])
             let zerosd = zeros.broadcasted(to: [1, 30, 30, 1])
             
-            let 𝛁generatorG = TensorFlow.gradient(at: generatorG) { g -> Tensorf in
+            let (gLoss, 𝛁generatorG) = valueWithGradient(at: generatorG) { g -> Tensorf in
                 let fakeY = g(realX)
                 let cycledX = generatorF(fakeY)
                 let fakeX = generatorF(realY)
                 let cycledY = g(fakeX)
-                
+
                 let cycleConsistencyLoss = (abs(realX - cycledX).mean() +
                                             abs(realY - cycledY).mean()) * lambdaL1
 
                 let discFakeY = discriminatorY(fakeY)
                 let generatorLoss = sigmoidCrossEntropy(logits: discFakeY, labels: onesd)
-                
+
                 let sameY = g(realY)
                 let identityLoss = abs(sameY - realY).mean() * lambdaL1 * 0.5
-                
+
                 let totalLoss = cycleConsistencyLoss + generatorLoss + identityLoss
-                ganGLossTotal += totalLoss
-                                                                    
+
                 return totalLoss
             }
-            
-            let 𝛁generatorF = TensorFlow.gradient(at: generatorF) { g -> Tensorf in
+
+            let (fLoss, 𝛁generatorF) = valueWithGradient(at: generatorF) { g -> Tensorf in
                 let fakeX = g(realY)
                 let cycledY = generatorG(fakeX)
                 let fakeY = generatorG(realX)
                 let cycledX = g(fakeY)
-                
+
                 let cycleConsistencyLoss = (abs(realY - cycledY).mean()
                                             + abs(realX - cycledX).mean()) * lambdaL1
 
                 let discFakeX = discriminatorX(fakeX)
                 let generatorLoss = sigmoidCrossEntropy(logits: discFakeX, labels: onesd)
-                
+
                 let sameX = g(realX)
                 let identityLoss = abs(sameX - realX).mean() * lambdaL1 * 0.5
-                
+
                 let totalLoss = cycleConsistencyLoss + generatorLoss + identityLoss
-                ganFLossTotal += totalLoss
+
                 return totalLoss
             }
-            
-            let 𝛁discriminatorX = TensorFlow.gradient(at: discriminatorX) { d -> Tensorf in
+
+            let (xLoss, 𝛁discriminatorX) = valueWithGradient(at: discriminatorX) { d -> Tensorf in
                 let fakeX = generatorF(realY)
                 let discFakeX = d(fakeX)
                 let discRealX = d(realX)
-                
+
                 let totalLoss = 0.5 * (sigmoidCrossEntropy(logits: discFakeX, labels: zerosd)
                                        + sigmoidCrossEntropy(logits: discRealX, labels: onesd))
-                discYLossTotal += totalLoss
+
                 return totalLoss
             }
-            
-            let 𝛁discriminatorY = TensorFlow.gradient(at: discriminatorY) { d -> Tensorf in
+
+            let (yLoss, 𝛁discriminatorY) = valueWithGradient(at: discriminatorY) { d -> Tensorf in
                 let fakeY = generatorG(realX)
                 let discFakeY = d(fakeY)
                 let discRealY = d(realY)
-                
+
                 let totalLoss = 0.5 * (sigmoidCrossEntropy(logits: discFakeY, labels: zerosd)
                                        + sigmoidCrossEntropy(logits: discRealY, labels: onesd))
-                discXLossTotal += totalLoss
+                
                 return totalLoss
             }
-            
+
             optimizerGG.update(&generatorG, along: 𝛁generatorG)
             optimizerGF.update(&generatorF, along: 𝛁generatorF)
             optimizerDX.update(&discriminatorX, along: 𝛁discriminatorX)
             optimizerDY.update(&discriminatorY, along: 𝛁discriminatorY)
             
-            totalLossCount += 1
-        }
-        
-        if Int(totalLossCount) % 50 == 0 {
-            Context.local.learningPhase = .inference
-            for testBatch in trainDatasetA.dataset.batched(1) {
-                let result = generatorG(testBatch.image)
-                let images = result * 0.5 + 0.5
+            writer.addScalars(mainTag: "train_loss",
+                              taggedScalars: [
+                                "GeneratorG": gLoss.scalars[0],
+                                "GeneratorF": fLoss.scalars[0],
+                                "DiscriminatorX": xLoss.scalars[0],
+                                "DiscriminatorY": yLoss.scalars[0]
+                              ],
+                              globalStep: step)
+            
+            // MARK: Inference
+            
+            if step % options.sampleLogPeriod == 0 {
+                let fakeSample = generatorG(sampleImage) * 0.5 + 0.5
                 
-                let image = Image(tensor: images[0] * 255)
-                
-                let currentURL = Folder.current.url.appendingPathComponent("\(epoch).jpg")
-                
-                image.save(to: currentURL, format: .rgb)
-                
-                break
+                writer.addImages(tag: "reals", images: sampleImage * 0.5 + 0.5, globalStep: step)
+                writer.addImages(tag: "fakes", images: fakeSample, globalStep: step)
+                writer.flush()
             }
+            
+            step += 1
         }
     }
-    
-    let generatorLossG = ganGLossTotal / totalLossCount
-    let generatorLossF = ganFLossTotal / totalLossCount
-    let discriminatorLossX = discXLossTotal / totalLossCount
-    let discriminatorLossY = discYLossTotal / totalLossCount
-    
-    writer.addScalars(mainTag: "train_loss",
-                      taggedScalars: [
-                        "GeneratorG": generatorLossG.scalars[0],
-                        "GeneratorF": generatorLossF.scalars[0],
-                        "DiscriminatorX": discriminatorLossX.scalars[0],
-                        "DiscriminatorY": discriminatorLossY.scalars[0]
-                      ],
-                      globalStep: epoch)
-    
-    print("GeneratorG \(generatorLossG)")
-    print("GeneratorF \(generatorLossF)")
-    print("DiscriminatorX \(discriminatorLossX)")
-    print("DiscriminatorY \(discriminatorLossY)")
+}
+
+// MARK: Final test
+
+let testDatasetA = try Images(folder: testFolderA).dataset
+let testDatasetB = try Images(folder: testFolderB).dataset
+
+let zippedTest = zip(testDatasetA, testDatasetB)
+
+let aResultsFolder = try Folder.current.createSubfolderIfNeeded(at: "testA_results")
+let bResultsFolder = try Folder.current.createSubfolderIfNeeded(at: "testB_results")
+
+var testStep = 0
+for testBatch in zippedTest.batched(1) {
+    withDevice(.gpu, options.gpuIndex) {
+        let realX = testBatch.first.image / 127.5 - 1
+        let realY = testBatch.first.image / 127.5 - 1
+        
+        let fakeY = generatorG(realX)
+        let fakeX = generatorF(realY)
+        
+        let resultX = realX.concatenated(with: fakeY, alongAxis: 2) * 0.5 + 0.5
+        let resultY = realY.concatenated(with: fakeX, alongAxis: 2) * 0.5 + 0.5
+        
+        let imageX = Image(tensor: resultX[0] * 255)
+        let imageY = Image(tensor: resultY[0] * 255)
+        
+        imageX.save(to: aResultsFolder.url.appendingPathComponent(String(testStep), isDirectory: false))
+        imageY.save(to: bResultsFolder.url.appendingPathComponent(String(testStep), isDirectory: false))
+        
+        testStep += 1
+    }
 }

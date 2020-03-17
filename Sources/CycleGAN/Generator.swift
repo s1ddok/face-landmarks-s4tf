c1d47bf549e7330e7de1370c1fa3d532149849fd
s1ddok <siddok@gmail.com>
2020-03-17 15:30:51 +0300

    introduce resnet generator

--- Sources/CycleGAN/Generator.swift
+++ Sources/CycleGAN/Generator.swift
@@ -61,3 +61,118 @@ public struct NetG<NT: FeatureChannelInitializable>: Layer where NT.TangentVecto
         return self.module(input)
     }
 }
+
+public struct ResNetGenerator<NT: FeatureChannelInitializable>: Layer where NT.TangentVector.VectorSpaceScalar == Float, NT.Input == Tensorf, NT.Output == Tensorf {
+    
+    var conv1: Conv2D<Float>
+    var norm1: NT
+    
+    var conv2: Conv2D<Float>
+    var norm2: NT
+    
+    var conv3: Conv2D<Float>
+    var norm3: NT
+    
+    var resblocks: [ResnetBlock<NT>]
+    
+    var upConv1: TransposedConv2D<Float>
+    var upNorm1: NT
+    
+    var upConv2: TransposedConv2D<Float>
+    var upNorm2: NT
+    
+    var lastConv: Conv2D<Float>
+    
+    public init(inputChannels: Int,
+                outputChannels: Int,
+                blocks: Int,
+                ngf: Int,
+                normalization: NT.Type,
+                useDropout: Bool = false) {
+        self.norm1 = .init(featureCount: ngf)
+        let useBias = self.norm1 is InstanceNorm2D<Float>
+        
+        let filterInit: (TensorShape) -> Tensorf = { Tensorf(randomNormal: $0, standardDeviation: Tensorf(0.02)) }
+        let biasInit: (TensorShape) -> Tensorf = useBias ? zeros() : filterInit
+        
+        self.conv1 = .init(filterShape: (7, 7,
+                                         inputChannels, ngf),
+                           strides: (1, 1),
+                           filterInitializer: filterInit,
+                           biasInitializer: biasInit)
+        
+        var mult = 1
+        
+        self.conv2 = .init(filterShape: (3, 3,
+                                         ngf * mult, ngf * mult * 2),
+                           strides: (2, 2),
+                           filterInitializer: filterInit,
+                           biasInitializer: biasInit)
+        self.norm2 = .init(featureCount: ngf * mult * 2)
+        
+        mult = 2
+        
+        self.conv3 = .init(filterShape: (3, 3,
+                                         ngf * mult, ngf * mult * 2),
+                           strides: (2, 2),
+                           filterInitializer: filterInit,
+                           biasInitializer: biasInit)
+        self.norm3 = .init(featureCount: ngf * mult * 2)
+        
+        mult = 4
+        
+        self.resblocks = (0..<blocks).map { _ in
+            ResnetBlock(channels: ngf * mult,
+                        paddingMode: .reflect,
+                        normalization: normalization,
+                        useDropOut: useDropout,
+                        filterInit: filterInit,
+                        biasInit: biasInit)
+        }
+        
+        mult = 4
+        
+        self.upConv1 = .init(filterShape: (3, 3, ngf * mult, ngf * mult / 2),
+                             strides: (1, 1),
+                             filterInitializer: filterInit,
+                             biasInitializer: biasInit)
+        self.upNorm1 = .init(featureCount: ngf * mult / 2)
+        
+        mult = 2
+        
+        self.upConv2 = .init(filterShape: (3, 3, ngf * mult, ngf * mult / 2),
+                             strides: (1, 1),
+                             filterInitializer: filterInit,
+                             biasInitializer: biasInit)
+        self.upNorm2 = .init(featureCount: ngf * mult / 2)
+        
+        self.lastConv = .init(filterShape: (7, 7, ngf, outputChannels),
+                              filterInitializer: filterInit,
+                              biasInitializer: biasInit)
+    }
+    
+    @differentiable
+    public func callAsFunction(_ input: Tensorf) -> Tensorf {
+        var x = input.padded(forSizes: [(0, 0), (3, 3), (3, 3), (0, 0)], mode: .reflect)
+        x = x.sequenced(through: conv1, norm1)
+        x = relu(x)
+        x = x.sequenced(through: conv2, norm2)
+        x = relu(x)
+        x = x.sequenced(through: conv3, norm3)
+        x = relu(x)
+        
+        x = resblocks(x)
+        
+        x = x.sequenced(through: upConv1, upNorm1)
+        x = relu(x)
+        x = x.sequenced(through: upConv2, upNorm2)
+        x = relu(x)
+        
+        x = x.padded(forSizes: [(0, 0), (3, 3), (3, 3), (0, 0)], mode: .reflect)
+        x = lastConv(x)
+        x = tanh(x)
+        
+        return x
+    }
+}
+

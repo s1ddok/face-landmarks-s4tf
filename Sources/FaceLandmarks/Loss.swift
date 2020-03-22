193dee0c9c0e8a73d8bf60c1a2723aa8d916dc2e
s1ddok <siddok@gmail.com>
2020-03-22 12:11:45 +0300

    introduce wing loss

---
+++ Sources/FaceLandmarks/Loss.swift
@@ -0,0 +1,16 @@
+import TensorFlow
+
+@differentiable(wrt: predicted)
+func wingLoss(predicted: Tensorf, expected: Tensorf, w: Float = 10, eps: Float = 0.02) -> Tensorf {
+    let x = abs(predicted - expected)
+    let center = w * log(1 + x / eps)
+    let c = w - w * log(1 + w / eps)
+    let wings = x - c
+    
+    // There is a more elegant way but it is not differentiable as of now
+    // Tensorf(x .< w) * center + Tensorf(x .>= w) * wings
+    var loss = max(sign(w - x), 0) * center
+    loss = loss + max(sign(x - w), 0) * wings
+    return loss.mean()
+}
+
